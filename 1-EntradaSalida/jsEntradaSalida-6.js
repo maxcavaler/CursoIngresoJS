3e5ec20935a242315658f27e784ecb04c1968a2b
alumno <alumno>
2019-02-05 21:29:09 -0300

    ejercicio6y7

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,9 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
-
+{   
+    var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert("el resultado es " + numero + numero2);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,29 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert("su resultado es " + numero + numero2);
 }
 
 function restar()
 {
-	
+	var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert(numero - numero2);
 }
 
 function multiplicar()
 { 
-	
+	var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert("la multiplicacion da como resultado " + numero * numero2);
 }
 
 function dividir()
 {
-	
+	var numero = parseInt(document.getElementById("numeroUno").value);
+    var numero2 = parseInt(document.getElementById("numeroDos").value);
+    alert("la division de estos numero es " + numero / numero2);
 }
 
